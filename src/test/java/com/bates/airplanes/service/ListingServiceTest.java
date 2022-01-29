9536e1cfe9d72f0600786e4d6d7ddf8f50e976de
Bates <GDB0204@daugherty.com>
2022-01-29 14:29:05 -0600

    Added spy testing for listingService

---
+++ src/test/java/com/bates/airplanes/service/ListingServiceTest.java
@@ -0,0 +1,63 @@
+package com.bates.airplanes.service;
+
+import com.bates.airplanes.model.Listing;
+import com.bates.airplanes.model.ScrapeSource;
+import com.bates.airplanes.repository.ListingRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ListingServiceTest {
+
+    @InjectMocks
+    ListingService listingService;
+
+    @Mock
+    ListingCalculationService listingCalculationService;
+
+    @Mock
+    ListingRepository listingRepository;
+
+    @Mock
+    ScrapeServiceFactory scrapeServiceFactory;
+
+    @Mock
+    ScrapeService scrapeService;
+
+    @Test
+    public void testServiceCallsCorrectOtherServices() {
+        //Arrange Mocks
+        when(scrapeService.getWebListings()).thenReturn(new ArrayList<>());
+        when(scrapeServiceFactory.getScrapeService(any())).thenReturn(scrapeService);
+        when(listingRepository.get()).thenReturn(new ArrayList<>());
+        when(listingCalculationService.calculateExpiredSavedListings(any())).thenReturn(new ArrayList<>());
+        when(listingCalculationService.calculateNewWebListings(any())).thenReturn(new ArrayList<>());
+
+        //Act
+        listingService.fetchActiveListings();
+
+        //Assert
+        verify(scrapeServiceFactory, atLeast(1)).getScrapeService(any());
+        verify(listingRepository, times(2)).get();
+        verify(listingCalculationService, times(1)).calculateNewWebListings(any());
+        verify(listingCalculationService, times(1)).calculateExpiredSavedListings(any());
+        verify(scrapeService, atLeast(1)).getWebListings();
+
+    }
+
+}
